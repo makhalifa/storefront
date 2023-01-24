69b97c1177a2fb135198e669b1700e8fc43ba0d0
Mohamed <7mdiano@gmail.com>
2023-01-24 17:28:01 +0200

    add some test cases

--- src/tests/api/order.spec.ts
+++ src/tests/api/order.spec.ts
@@ -2,9 +2,28 @@ import supertest from 'supertest'
 import app from '../../index'
 
 const req = supertest(app)
+let token: string
 
 describe('GET /orders', () => {
     it('should return a 200 response', async () => {
+        // create a user returning
+        const res = await req.post('/users').send({
+            firstname: 'test',
+            lastname: 'test',
+            password: 'test'
+        })
+        token = res.text
+        console.log(`Bearer ${token}`)
+        token = token.substring(1, res.text.length - 1)
+        console.log(`Bearer ${token}`)
+        
+        // set some headers in request
+        const res2 = await req.get('/orders').set('Authorization', `Bearer ${token}`)
+        // print headers of response
+        expect(res2.status).toBe(200)
+    })
+
+    it('should return a 401 response', async () => {
         // set the authorization header
         const res = await req.get('/orders')
         expect(res.status).toBe(401)
@@ -13,12 +32,25 @@ describe('GET /orders', () => {
 
 describe('GET /orders/:id', () => {
     it('should return a 200 response', async () => {
+        const res = await req.get('/orders/1').set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(200)
+    })
+
+    it('should return a 401 response', async () => {
         const res = await req.get('/orders/1')
         expect(res.status).toBe(401)
     })
 })
 
 describe('POST /orders', () => {
+    it('should return a 200 response', async () => {
+        const res = await req.post('/orders').set('Authorization', `Bearer ${token}`).send({
+            user_id: 1,
+            status: 'active'
+        })
+        expect(res.status).toBe(200)
+    })
+
     it('should return a 401 response', async () => {
         const res = await req.post('/orders').send({
             user_id: 1,
@@ -29,25 +61,13 @@ describe('POST /orders', () => {
 })
 
 describe('DELETE /orders/:id', () => {
+    it('should return a 200 response', async () => {
+        const res = await req.delete('/orders/1').set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(200)
+    })
+
     it('should return a 401 response', async () => {
         const res = await req.delete('/orders/1')
-        expect(res.status).toBe(401)
-    })
-})
-
-describe('POST /orders/:id/products', () => {
-    it('should return a 401 response', async () => {
-        const res = await req.post('/orders/1/products').send({
-            product_id: 1,
-            quantity: 1
-        })
-        expect(res.status).toBe(401)
-    })
-})
-
-describe('GET /orders/:id/products', () => {
-    it('should return a 401 response', async () => {
-        const res = await req.get('/orders/1/products')
         expect(res.status).toBe(401)
     })
 })
--- src/tests/api/product.spec.ts
+++ src/tests/api/product.spec.ts
@@ -2,6 +2,7 @@ import supertest from 'supertest'
 import app from '../../index'
 
 const req = supertest(app)
+let token: string
 
 describe('GET /products', () => {
     it('should return a 200 response', async () => {
@@ -18,6 +19,22 @@ describe('GET /products/:id', () => {
 })
 
 describe('POST /products', () => {
+    it('should return a 200 response', async () => {
+        const resUser = await req.post('/users').send({
+            firstname: 'test',
+            lastname: 'test',
+            password: 'test'
+        })
+        token = resUser.text.substring(1, resUser.text.length - 1)
+
+        const res = await req.post('/products').send({
+            name: 'test',
+            price: 10,
+            category: 'test'
+        }).set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(200)
+    })
+
     it('should return a 401 response for Authentication', async () => {
         const res = await req.post('/products').send({
             name: 'test',
@@ -29,6 +46,11 @@ describe('POST /products', () => {
 })
 
 describe('DELETE /products/:id', () => {
+    it('should return a 200 response', async () => {
+        const res = await req.delete('/products/1').set('Authorization', `Bearer ${token}`)
+        expect(res.status).toBe(200)
+    })
+
     it('should return a 401 response for Authentication', async () => {
         const res = await req.delete('/products/1')
         expect(res.status).toBe(401)
